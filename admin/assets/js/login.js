28ffccd70666696d71f4a76ce22000e10618433e
Elxan <elx4n13@gmail.com>
2023-03-05 13:09:02 +0400

    adminn

--- admin/assets/js/login.js
+++ admin/assets/js/login.js
@@ -1,10 +1,12 @@
 import { db } from "./connection.js";
 import { get,set,ref,onValue,push } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-database.js";
-
+if(localStorage.getItem('user')){
+  window.location.assign('./../../../admin/panel.html');
+}
 const btn = document.querySelector(".login__form button");
 const user = document.querySelector(".login__form #user");
 const pass = document.querySelector(".login__form #pass");
-
+let data=await get(ref(db,'/library/login'))
 btn.addEventListener("click", async(e) => {
   if (user.value === "" && pass.value === "") {
     alert("user və parol daxil edilməyib");
@@ -18,6 +20,7 @@ btn.addEventListener("click", async(e) => {
   let data=await get(ref(db,'/library/login'))
   data=data.val();
   if(data.username===user.value && data.password===parseInt(pass.value)){
+  localStorage.setItem("user", data.username);   // Locala yazdirma
    window.location.assign('./../../../admin/panel.html');
   }
   else{
