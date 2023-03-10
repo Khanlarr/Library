eb848d19b4d16fb993ddac93dce0c5bb17987a01
Khanlarr <xanlar.beybalayev@gmail.com>
2023-03-10 19:32:35 +0400

    grbfd

--- assets/js/aboutStore.js
+++ assets/js/aboutStore.js
@@ -75,7 +75,7 @@ document.querySelectorAll(".header__list ul li a").forEach((e) => {
       t.shift();
     }
   }
-  console.log(window.location.pathname.split("/"));
+  console.log(window.location.pathname.split("/")[2].split(".")[0].toLowerCase(),s);
   if (
     s === window.location.pathname.split("/")[2].split(".")[0].toLowerCase()
   ) {
